033bd83cf3676e9dc7b0307f3445379a91104246
David Wang <davidwang@qnap.com>
2025-11-19 16:01:50 +0800

    deploy webapp on github

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Portfolio/', // base url on github pages
       server: {
         port: 3000,
         host: '0.0.0.0',
